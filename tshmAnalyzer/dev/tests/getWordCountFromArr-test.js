c6909cbdd515de144836c314de066ce534de425b
Jesen Tanadi <jesen@Jesens-MBP.home>
2018-03-30 07:27:06 -0400

    remove .only

--- tshmAnalyzer/dev/tests/getWordCountFromArr-test.js
+++ tshmAnalyzer/dev/tests/getWordCountFromArr-test.js
@@ -1,11 +1,11 @@
 const test = require("tape");
-const getWordCount = require("../getWordCountFromArr")
+const getWordCount = require("../getWordCountFromArr");
 
-test.only("works", t => {
+test("counts words in an array", t => {
   const input = ["hello how are you great", "that's cool, not so bad", "yay thank you!"];
   const actual = getWordCount(input);
   const expected = 13;
 
   t.equal(actual, expected);
   t.end();
-})
+});
