cd69a127e170da73c35244461d5a928f25c221f3
Jesen Tanadi <jesen@Jesens-MBP.home>
2018-03-29 22:33:41 -0400

    clean up, linting

--- tshmAnalyzer/build/_/app.js
+++ tshmAnalyzer/build/_/app.js
@@ -1,9 +1,12 @@
+// Because some funcs are made available through HTML imports...
+/* eslint-disable no-undef */
+
 let CHOSENFUNC;
 const funcRadio = document.getElementsByName("funcToDo");
-const contentInput = document.getElementById("rawContent"); 
+const contentInput = document.getElementById("rawContent");
 const regexInput = document.getElementById("regexInput");
 const thresholdInput = document.getElementById("thresholdInput");
-const button = document.querySelector("input[type='button']")
+const button = document.querySelector("input[type='button']");
 
 // Collection of available functions
 const funcsToRun = {
@@ -12,29 +15,29 @@ const funcsToRun = {
   showTooLong,
 };
 
-const radioChecked = (radioGroup) => {
+const radioChecked = radioGroup => {
   let answer = false;
   radioGroup.forEach(radio => {
     if(radio.checked) answer = true;
-  })
+  });
   return answer;
 };
 
-funcRadio.forEach(radio => radio.addEventListener("change", (e) => {
+funcRadio.forEach(radio => radio.addEventListener("change", e => {
   // Each radio button's id corresponds to function name in funcsToRun
   CHOSENFUNC = e.target.id;
-}))
+}));
 
-button.addEventListener("click", () =>{
+button.addEventListener("click", () => {
   const text = paragraphsToArray(contentInput.value);
   let threshold;
 
   if(CHOSENFUNC === "showTooLong" && regexInput.value) {
     threshold = thresholdInput.value;
-  } 
+  }
 
   if(radioChecked(funcRadio) && regexInput.value) {
-    let arrToWrite = funcsToRun[CHOSENFUNC](text, regexInput.value, threshold);
+    const arrToWrite = funcsToRun[CHOSENFUNC](text, regexInput.value, threshold);
     document.querySelector("#output").value = arrToWrite.join("\n\n");
   }
 });
--- tshmAnalyzer/build/_/helperFuncs.js
+++ tshmAnalyzer/build/_/helperFuncs.js
@@ -1,23 +1,4 @@
-const paragraphsToArray = function(textInput, trimmed = true) {
-  const retArr = textInput.split("\n")
-    .filter(item => item)
-  if(!trimmed) return retArr;
-  return retArr.map(item => item.trim());
-};
-
-const filterOutRegex = function(inputArr, expression) {
-  let exp;
-  typeof expression === "string" ? exp = new RegExp(expression) : exp = expression;
-  return inputArr.filter(item => !exp.test(item));
-};
-
-const findByRegex = function(inputArr, expression) {
-  let exp;
-  typeof expression === "string" ? exp = new RegExp(expression) : exp = expression;
-  return inputArr.filter(item => exp.test(item));
-};
-
-const getIndices = function (inputArr, itemToIndex) {
+const getIndices = function(inputArr, itemToIndex) {
   if(!Array.isArray(inputArr)) throw new Error("First argument must be an array");
   const exp = new RegExp(itemToIndex);
 
@@ -38,37 +19,59 @@ const flatten = arr => arr.reduce((retArr, item) => {
   return retArr;
 }, []);
 
-const codeToObj = function(inputArr, regex) {
-  const codeIndices = getIndices(inputArr, regex)
-  const retObj = {}
-  for(let i = 0; i < codeIndices.length; i ++) {
+const codeToObj = function (inputArr, regex) {
+  const codeIndices = getIndices(inputArr, regex);
+  const retObj = {};
+  for(let i = 0; i < codeIndices.length; i++) {
     const code = inputArr[codeIndices[i]];
     const text = inputArr.slice(codeIndices[i] + 1, codeIndices[i + 1]);
     
-    (!retObj[code]) ? retObj[code] = text : retObj[code].push(text);
+    // (!retObj[code]) ? retObj[code] = text : retObj[code].push(text);
+    retObj[code] = text;
   }
   return retObj;
-}
+};
 
-const getWordCountFromArray = (inputArr) => {
-  return inputArr.reduce((sum, item) => {
-    return sum += item.split(" ").length
-  }, 0)
-}
+const getWordCountFromArray = inputArr => inputArr.reduce((sum, item) => {
+  return sum += item.split(" ").length;
+}, 0);
 
-const showTooLong = function(inputArr, regex, threshold) {
-  const contentObj = codeToObj(inputArr, regex)
+// /////////////////////////////////////
+// Functions directly used by app below
+// /////////////////////////////////////
+
+const paragraphsToArray = function(textInput, trimmed = true) {
+  const retArr = textInput.split("\n")
+    .filter(item => item);
+  if(!trimmed) return retArr;
+  return retArr.map(item => item.trim());
+};
+
+const filterOutRegex = function(inputArr, expression) {
+  let exp = expression;
+  if(typeof expression === "string") exp = new RegExp(expression);
+
+  return inputArr.filter(item => !exp.test(item));
+};
+
+const findByRegex = function(inputArr, expression) {
+  let exp = expression;
+  if(typeof expression === "string") exp = new RegExp(expression);
+  
+  return inputArr.filter(item => exp.test(item));
+};
+
+const showTooLong = function (inputArr, regex, threshold) {
+  const contentObj = codeToObj(inputArr, regex);
 
   const longCodes = [];
-  for(let key in contentObj) {
+  for(const key in contentObj) {
     // SOMETHING WEIRD HERE. WHY DO I NEED TO FLATTEN?
     const contentArray = flatten(contentObj[key]);
     
     const wordCount = getWordCountFromArray(contentArray);
-    if(wordCount > threshold) longCodes.push(key)
+    if(wordCount > threshold) longCodes.push(key);
   }
 
-  return longCodes.map((code) => {
-    return `${code}\n${contentObj[code].join("\n\n")}`;
-  })
-}
+  return longCodes.map(code => `${code}\n${contentObj[code].join("\n\n")}`);
+};
