c1aa7892c6bf2f72e430c2b38341a6300c434215
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-04-18 17:56:29 -0400

    work in progress sketch...

--- contentSeparator/dist/_/scroll.js
+++ contentSeparator/dist/_/scroll.js
@@ -3,38 +3,36 @@ let pgIndex = 0;
 const currentTransition = window.getComputedStyle(document.body).transition;
 
 const scrollToHeightIndex = (height, index, cssTransition = "none") => {
-	/* (num, num, string)
+  /* (num, num, string)
+  Using CSS's translate property, move the body to a target height
+  based on height * index. index works like page numbers if height = 1 "page"
+  cssTransition defaults to "none"
+  */
 
-	Using CSS's translate property, move the body to a target height 
-	based on height * index. index works like page numbers if height = 1 "page"
-
-	cssTransition defaults to "none"
-	*/
-	document.body.style.transform = `translate(0, -${height * index}px)`;
-	document.body.style.transition = cssTransition;
-}
+  document.body.style.transform = `translate(0, -${height * index}px)`;
+  document.body.style.transition = cssTransition;
+};
 
 window.addEventListener("resize", () => {
-	windowHeight = window.innerHeight;
+  windowHeight = window.innerHeight;
 
-	// Prevent transition-scrolling while resizing.
-	scrollToHeightIndex(windowHeight, pgIndex);
-})
-
-document.body.addEventListener("click", (evt) => {
-	if(evt.target.tagName === "P") {
-		if(evt.target.className.includes("down")) {
-			pgIndex ++;
-			scrollToHeightIndex(windowHeight, pgIndex, currentTransition);
-		
-		} else if(evt.target.className.includes("up")) {
-			pgIndex --;
-			scrollToHeightIndex(windowHeight, pgIndex, currentTransition);
-		}
-	}
+  // Prevent transition-scrolling while resizing.
+  scrollToHeightIndex(windowHeight, pgIndex);
 });
 
-document.querySelector(".top").addEventListener("click", (evt) => {
+document.body.addEventListener("click", evt => {
+  if(evt.target.tagName === "P") {
+    if(evt.target.className.includes("down")) {
+      pgIndex++;
+      scrollToHeightIndex(windowHeight, pgIndex, currentTransition);
+    } else if(evt.target.className.includes("up")) {
+      pgIndex--;
+      scrollToHeightIndex(windowHeight, pgIndex, currentTransition);
+    }
+  }
+});
+
+document.querySelector(".top").addEventListener("click", () => {
   pgIndex = 0;
   scrollToHeightIndex(windowHeight, pgIndex, currentTransition);
-})
+});
