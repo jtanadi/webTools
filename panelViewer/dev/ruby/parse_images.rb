c0863d1bfc64e8b727715c30670698e7d115c323
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-04-30 12:00:18 -0400

    working version

---
+++ panelViewer/dev/ruby/parse_images.rb
@@ -0,0 +1,37 @@
+ACCEPTEDFORMAT = [".jpg", ".jpeg", ".gif", ".png", ".tif", ".tiff", ".tga", ".bmp"]
+
+def parse_images(directory, panels_arr)
+  ret_string = "<div class=\"panel_row\">"
+
+  files = Dir.entries(directory)
+  images = files.select { |file| ACCEPTEDFORMAT.include? File.extname(file) }
+  
+  images.each.with_index do |image, index|
+    if index == 0
+      ret_string += "<div class=\"panel_row\">"
+    # elsif index % 5 == 0 && index != 0
+    #   ret_string += "</div><div class=\"panel_row\">"
+    end
+
+    alt_name = File.basename(image, ".*")
+    ret_string += "<div class=\"panel_cell\">"
+    ret_string += "<img src=\"./#{image}\" class=\"panel\" alt=\"#{alt_name}\" "
+
+    panels_arr.each do |panel|
+      if panel[:code] == alt_name
+        panel[:selectors].each do |selector_num, selector_data|
+          data_name = "data-" + selector_num.to_s
+          data_values = []
+          selector_data.each do |key, val|
+            data_values << key.to_s
+          end
+          ret_string += "#{data_name}=\"#{data_values.join(", ")}\" "
+        end
+      end
+    end
+
+    ret_string += "></div>"
+  end
+  ret_string += "</div></div>"
+  ret_string  
+end
